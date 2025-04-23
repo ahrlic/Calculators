7d448ef6c9f2de4ec620fe7bf9e97bf022d28472
Ariana Hrlic <ariana.hrlic@gmail.com>
2025-04-23 10:19:21 -0400

    Update CompoundInvestment.java comments

--- CompoundInvestment.java
+++ CompoundInvestment.java
@@ -25,23 +25,15 @@ public class CompoundInvestment {
     regularPayment = Console.readDouble("What is the regular payment?");
     totalYears = Console.readInt("How many years are you investing?");
 
-    // Initialize 2D array - adding an extra year so the output will match the
-    // sample table
-    // The first row will contain all zeros - this is intentional!
-    // Our 'year' will now match the array index
     investment = new double[totalYears + 1][3];
 
     // Populate the array with the calculations
     for (int year = 1; year <= totalYears; year++) {
 
- 
-      // TODO: Calculate the value of first column (initial)
     	investment[year][INITIAL] = regularPayment + investment[year-1][BALANCE];
-    	
-      // TODO: Calculate the value of second column (interest)
+    
     	investment[year][INTEREST] = (rate / 100) * investment[year][INITIAL];
     	
-      // TODO: Calculate the value of the third column (balance)
     	investment[year][BALANCE] = investment[year-1][BALANCE] + investment[year][INITIAL];
     	
     }  
